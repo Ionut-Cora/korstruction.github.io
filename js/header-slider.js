59a1d300fdebc482099f73d6f9b9df4f888cd9d6
Ionut Cora <cora.ionut92@yahoo.com>
2023-04-07 13:27:26 +0100

    homepage js fixed

---
+++ js/header-slider.js
@@ -0,0 +1,98 @@
+
+// COLORS VARIABLES
+const fifthColor = '#A0937D';
+
+const picture1 = document.querySelector('.picture1');
+const picture2 = document.querySelector('.picture2');
+const picture3 = document.querySelector('.picture3');
+
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+
+const pic1 = document.getElementById('pic1');
+const pic2 = document.getElementById('pic2');
+const pic3 = document.getElementById('pic3');
+
+function handlePics1() {
+    pic1.style.backgroundColor = "#fff";
+    pic2.style.backgroundColor = fifthColor;
+    pic3.style.backgroundColor = fifthColor;
+}
+
+function handlePics2() {
+    pic2.style.backgroundColor = "#fff";
+    pic1.style.backgroundColor = fifthColor;
+    pic3.style.backgroundColor = fifthColor;
+}
+
+function handlePics3() {
+    pic3.style.backgroundColor = "#fff";
+    pic1.style.backgroundColor = fifthColor;
+    pic2.style.backgroundColor = fifthColor;
+}
+
+function handleDesign() {
+    if (picture1.classList.contains('active')) {
+        picture1.classList.remove('active');
+        picture2.classList.add('active');
+        handlePics2();
+    } else if (picture2.classList.contains('active')) {
+        picture2.classList.remove('active');
+        picture3.classList.add('active');
+        handlePics3();
+    } else {
+        picture3.classList.remove('active');
+        picture1.classList.add('active');
+        handlePics1();
+    }
+}
+
+let refreshInterval;
+
+refreshInterval = setInterval(function() {
+    handleDesign();
+}, 5000);
+
+prev.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    if (picture1.classList.contains('active')) {
+        picture1.classList.remove('active');
+        picture3.classList.add('active');
+        handlePics2();
+    } else if (picture3.classList.contains('active')) {
+        picture3.classList.remove('active');
+        picture2.classList.add('active');
+        handlePics3
+    } else {
+        picture2.classList.remove('active');
+        picture1.classList.add('active');
+        handlePics1();
+    }
+});
+
+next.addEventListener('click', (event) => {
+    event.preventDefault();
+    handleDesign();
+});
+
+pic1.addEventListener('click', () => {
+    picture2.classList.remove('active');
+    picture3.classList.remove('active');
+    picture1.classList.add('active');
+    handlePics1()
+});
+
+pic2.addEventListener('click', () => {
+    picture1.classList.remove('active');
+    picture3.classList.remove('active');
+    picture2.classList.add('active');
+    handlePics2();
+});
+
+pic3.addEventListener('click', () => {
+    picture1.classList.remove('active');
+    picture2.classList.remove('active');
+    picture3.classList.add('active');
+    handlePics3();
+});
--- js/script.js
+++ js/script.js
@@ -74,9 +74,22 @@ const galleryPageDonePictures = [
     }
 ];
 
-// COLORS VARIABLES
+// NAVIGATION
+const menuBtn = document.getElementById('menu-btn');
+const nav = document.querySelector('nav');
+let isClicked = false;
 
-const fifthColor = '#A0937D';
+menuBtn.addEventListener('click', () => {
+    if (!isClicked) {
+        nav.style.display = 'block';
+        isClicked = true;
+    } else {
+        nav.style.display = 'none';
+        isClicked = false;
+    }
+}, false);
+
+
 
 // const root = document.getElementById('root');
 // window.onload = () => {
@@ -228,63 +241,63 @@ const fifthColor = '#A0937D';
 
 // };
 
-const picture1 = document.querySelector('.picture1');
-const picture2 = document.querySelector('.picture2');
-const picture3 = document.querySelector('.picture3');
+// const picture1 = document.querySelector('.picture1');
+// const picture2 = document.querySelector('.picture2');
+// const picture3 = document.querySelector('.picture3');
 
-const prev = document.getElementById('prev');
-const next = document.getElementById('next');
+// const prev = document.getElementById('prev');
+// const next = document.getElementById('next');
 
-const pic1 = document.getElementById('pic1');
-const pic2 = document.getElementById('pic2');
-const pic3 = document.getElementById('pic3');
+// const pic1 = document.getElementById('pic1');
+// const pic2 = document.getElementById('pic2');
+// const pic3 = document.getElementById('pic3');
 
-function handlePics1() {
-    pic1.style.backgroundColor = "#fff";
-    pic2.style.backgroundColor = fifthColor;
-    pic3.style.backgroundColor = fifthColor;
-}
+// function handlePics1() {
+//     pic1.style.backgroundColor = "#fff";
+//     pic2.style.backgroundColor = fifthColor;
+//     pic3.style.backgroundColor = fifthColor;
+// }
 
-function handlePics2() {
-    pic2.style.backgroundColor = "#fff";
-    pic1.style.backgroundColor = fifthColor;
-    pic3.style.backgroundColor = fifthColor;
-}
+// function handlePics2() {
+//     pic2.style.backgroundColor = "#fff";
+//     pic1.style.backgroundColor = fifthColor;
+//     pic3.style.backgroundColor = fifthColor;
+// }
 
-function handlePics3() {
-    pic3.style.backgroundColor = "#fff";
-    pic1.style.backgroundColor = fifthColor;
-    pic2.style.backgroundColor = fifthColor;
-}
+// function handlePics3() {
+//     pic3.style.backgroundColor = "#fff";
+//     pic1.style.backgroundColor = fifthColor;
+//     pic2.style.backgroundColor = fifthColor;
+// }
 
-function handleDesign() {
-    if (picture1.classList.contains('active')) {
-        picture1.classList.remove('active');
-        picture2.classList.add('active');
-        handlePics2();
-    } else if (picture2.classList.contains('active')) {
-        picture2.classList.remove('active');
-        picture3.classList.add('active');
-        handlePics3();
-    } else {
-        picture3.classList.remove('active');
-        picture1.classList.add('active');
-        handlePics1();
-    }
-}
+// function handleDesign() {
+//     if (picture1.classList.contains('active')) {
+//         picture1.classList.remove('active');
+//         picture2.classList.add('active');
+//         handlePics2();
+//     } else if (picture2.classList.contains('active')) {
+//         picture2.classList.remove('active');
+//         picture3.classList.add('active');
+//         handlePics3();
+//     } else {
+//         picture3.classList.remove('active');
+//         picture1.classList.add('active');
+//         handlePics1();
+//     }
+// }
 
-let refreshInterval;
+// let refreshInterval;
 
-refreshInterval = setInterval(function() {
-    handleDesign();
-}, 1000);
+// refreshInterval = setInterval(function() {
+//     handleDesign();
+// }, 1000);
 
-window.onbeforeunload = (e) => {
+// window.onbeforeunload = (e) => {
 
-    console.log('close window');
-    clearInterval(refreshInterval);
-    refreshInterval = 0;
-};  
+//     console.log('close window');
+//     clearInterval(refreshInterval);
+//     refreshInterval = 0;
+// };  
 
 // window.addEventListener('beforeunload', () => {
 //     clearInterval(refreshInterval);
@@ -342,12 +355,12 @@ window.onbeforeunload = (e) => {
 //     `
 // });
 
-const logo = document.getElementById('logo');
-const homeBtn = document.getElementById('home-btn');
-const aboutBtn = document.getElementById('about-btn');
-const galleryBtn = document.getElementById('gallery-btn');
-const contactBtn = document.getElementById('contact-btn');
-const fullContent = document.getElementById('full-content');
+// const logo = document.getElementById('logo');
+// const homeBtn = document.getElementById('home-btn');
+// const aboutBtn = document.getElementById('about-btn');
+// const galleryBtn = document.getElementById('gallery-btn');
+// const contactBtn = document.getElementById('contact-btn');
+// const fullContent = document.getElementById('full-content');
 
 // home page btns
 
@@ -385,19 +398,7 @@ const fullContent = document.getElementById('full-content');
 //     });
 // });
 
-    const menuBtn = document.getElementById('menu-btn');
-    const nav = document.querySelector('nav');
-    let isClicked = false;
-
-    menuBtn.addEventListener('click', () => {
-        if (!isClicked) {
-            nav.style.display = 'block';
-            isClicked = true;
-        } else {
-            nav.style.display = 'none';
-            isClicked = false;
-        }
-    }, false);
+    
 
     
 
