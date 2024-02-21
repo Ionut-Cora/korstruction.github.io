d6a713e0e8dcf899c0b16513d38b89fd20262075
Ionut Cora <cora.ionut92@yahoo.com>
2024-02-21 12:11:22 +0000

    menu icon and top logo fixed for all pages

--- js/script.js
+++ js/script.js
@@ -6,4 +6,10 @@ const body = document.querySelector('body');
 menuBtn.addEventListener('click', () => {
     nav.classList.toggle('toggle-style');
     body.classList.toggle('nav-open');
+
+    if (body.className === 'nav-open') {
+        menuBtn.name = 'close-outline';
+    } else {
+        menuBtn.name = 'menu-outline';
+    }
 });
