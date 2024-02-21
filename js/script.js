71c9d86c209c3e2de1182c4d701b9894f319a47d
Ionut Cora <cora.ionut92@yahoo.com>
2024-02-21 11:19:43 +0000

    top menu open fixed

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-
 // NAVIGATION
 const menuBtn = document.getElementById('menu-btn');
 const nav = document.querySelector('nav');
+const body = document.querySelector('body');
 
 menuBtn.addEventListener('click', () => {
     nav.classList.toggle('toggle-style');
+    body.classList.toggle('nav-open');
 });
